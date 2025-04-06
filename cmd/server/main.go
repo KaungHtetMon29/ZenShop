5f5df38225e19f68301ecc145384ee6a108753c3
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2025-04-06 14:49:57 +0700

    server setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"go_boilerplate/internal/routes"
+	"net/http"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 func main() {
-	
+	dsn := "host=localhost user=postgres password=12345678 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connected to database")
+	router:= routes.NewRouter()
+	router.AddRoute("GET", "/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Print("GET /users")
+	})
+	router.AddRoute("GET", "/items", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Print("GET /users")
+	})
+	server:=http.Server{Addr: ":8080",Handler: router}
+	fmt.Println("Starting server on port 8080")
+	fmt.Println("🔥 main() started")
+	if err := server.ListenAndServe(); err != nil {
+		panic("failed to start server")
+	}
 }
---
+++ internal/routes/router.go
@@ -0,0 +1,30 @@
+package routes
+
+import "net/http"
+type router struct { 
+	routes map[string]map[string]http.HandlerFunc
+}
+func NewRouter() *router {
+	return &router{
+		routes: make(map[string]map[string]http.HandlerFunc),
+	}
+}
+
+func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path:=req.URL.Path
+	method:=req.Method
+	if handlers, ok := r.routes[path]; ok {
+		if handler, ok := handlers[method]; ok {
+			handler(w, req)
+			return
+		}
+	}
+	http.NotFound(w, req)
+}
+
+func (r *router) AddRoute(method, path string, handler http.HandlerFunc) {
+	if r.routes[path]==nil{
+		r.routes[path]=make(map[string]http.HandlerFunc)
+	}
+	r.routes[path][method]=handler
+}
